656a66ac99cfc8532c4b5d747aa213a16dad4380
Minh Dong <136040904+MDong-06@users.noreply.github.com>
2023-07-09 07:09:19 +0700

    Update logger.js

--- logger.js
+++ logger.js
@@ -2,15 +2,15 @@ const chalk = require('chalk');
 //const notifier = require('node-notifier');
 
 module.exports = (str, end) => {
-	console.log(chalk.hex('#00FFFF').bold(`${end || '[ FCA-BRASL ]'} > `) + str);
+	console.log(chalk.hex('#00FFFF').bold(`${end || '[ FCA-DONGDEV ]'} > `) + str);
 };
 module.exports.onLogger = (str,end,ctscolor) => { 
 	var checkbutdak = ctscolor.replace("#",'');
 	if (ctscolor.indexOf('#') != 1) {
-		console.log(chalk.hex('#00FFFF').bold(`${end || '[ FCA-BRASL ]'} > `) + str);
+		console.log(chalk.hex('#00FFFF').bold(`${end || '[ FCA-DONGDEV ]'} > `) + str);
 	}
 	else if (!isNaN(checkbutdak)) {
-		console.log(chalk.hex(ctscolor).bold(`${end || '[ FCA-BRASL ]'} > `) + str);
+		console.log(chalk.hex(ctscolor).bold(`${end || '[ FCA-DONGDEV ]'} > `) + str);
 	} 
-	else console.log(chalk.hex('#00FFFF').bold(`${end || '[ FCA-BRASL ]'} > `) + str);
+	else console.log(chalk.hex('#00FFFF').bold(`${end || '[ FCA-DONGDEV ]'} > `) + str);
 }
